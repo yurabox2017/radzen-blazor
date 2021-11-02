65965af9c9a7a8ac94f059fdd4cbc62540b15ec9
YBokhov <bokhov@jamilco.ru>
2021-11-02 18:23:40 +0300

    no message

---
+++ Radzen.Blazor/Ut.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Radzen.Blazor
+{
+    internal static class Ut
+    {
+
+        internal static bool In<T>(this T sourse, params T[] values)
+        {
+            if (values == null)
+                throw new ArgumentNullException(nameof(values));
+
+            return values.Contains(sourse);
+        }
+    }
+}
